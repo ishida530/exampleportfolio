1956697bd5a90e10e06efe42bd05129ceebe5262
ishida530 <63319022+ishida530@users.noreply.github.com>
2020-08-31 19:44:13 +0200

    Add files via upload
    not rwd

---
+++ script.js
@@ -0,0 +1,52 @@
+const mainDiv = document.querySelector(".main");
+const aboutDiv = document.querySelector(".about");
+
+const portfolioDiv = document.querySelector(".portfolio");
+const contactDiv = document.querySelector(".contact");
+
+const main = document.getElementById('1').addEventListener("click", () => {
+
+
+    mainDiv.style.display = "block";
+    aboutDiv.style.display = "none";
+    portfolioDiv.style.display = "none";
+    contactDiv.style.display = "none"
+
+})
+const about = document.getElementById('2').addEventListener("click", () => {
+    console.log("dziala");
+    mainDiv.style.display = "none"
+    aboutDiv.style.display = "block"
+
+})
+const portfolio = document.getElementById('3').addEventListener("click", () => {
+
+
+    mainDiv.style.display = "none";
+    aboutDiv.style.display = "none";
+    portfolioDiv.style.display = "block";
+    contactDiv.style.display = "none"
+
+})
+const contact = document.getElementById('4').addEventListener("click", () => {
+
+
+    mainDiv.style.display = "none";
+    aboutDiv.style.display = "none";
+    portfolioDiv.style.display = "none";
+    contactDiv.style.display = "block"
+
+})
+const inputName = document.getElementById("name");
+const messageName = document.getElementById("message");
+const btn = document.querySelector("button").addEventListener("click", (e) => {
+
+    e.preventDefault()
+
+    if (inputName.value.length <= 2 || messageName.value.length <= 5) {
+        inputName.value = "";
+        messageName.value = "";
+        return alert("Wrong completed form!")
+    } else alert("Message was sent!")
+
+})
